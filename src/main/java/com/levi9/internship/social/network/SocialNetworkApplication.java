d2e9bc828704a7c91ef06fefb5ce889c4b8e5ca5
Uros Vesic <uros.vesic@levi9.com>
2024-04-23 17:55:15 +0200

    debug log

--- src/main/java/com/levi9/internship/social/network/SocialNetworkApplication.java
+++ src/main/java/com/levi9/internship/social/network/SocialNetworkApplication.java
@@ -1,18 +1,31 @@
 package com.levi9.internship.social.network;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @EnableScheduling
-@SpringBootApplication(scanBasePackages = { "com.levi9.internship.social.network" })
-public class SocialNetworkApplication
-{
+@SpringBootApplication(scanBasePackages = {"com.levi9.internship.social.network"})
+public class SocialNetworkApplication {
 
-	public static void main(final String[] args)
-	{
-		SpringApplication.run(SocialNetworkApplication.class, args);
+	@Value("${spring.cloud.aws.credentials.profile.name}")
+	private String profile;
+
+	private static final Logger log = LoggerFactory.getLogger(SocialNetworkApplication.class);
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SocialNetworkApplication.class, args);
+    }
+
+	@Bean
+	CommandLineRunner commandLineRunner(){
+		return args-> log.info("Profile: {}", profile);
 	}
 }
